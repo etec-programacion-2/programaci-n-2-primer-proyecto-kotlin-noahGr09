739f5b3fad476bcd098bda163a699f668197c2ab
Noah <noahlucianograna@gmail.com>
2025-04-25 10:04:21 -0300

    Paso 3: Control de flujo

--- app/src/main/kotlin/org/example/App.kt
+++ app/src/main/kotlin/org/example/App.kt
@@ -6,6 +6,22 @@ fun <T> mostrar_tipo(variable: T): String {
 fun area_rectangulo(base: Double, altura: Double): String{
     return "${base*altura}cm2"
 }
+fun par_o_impar(num: Int): String{
+    if (num%2 == 0) {
+        return "Par"
+    } else {
+        return "Impar"
+    }
+}
+fun positivo(num: Int): String{
+    if(num>0){
+        return "Positivo"
+    }else if (num<0){
+        return "negativo"
+    }else {
+        return "Cero"
+    }
+}
 fun main() {
     val nombre = "Noah"
     val num = 24
@@ -13,4 +29,6 @@ fun main() {
     val decimal = 3.14
     println(mostrar_tipo(bool))
     println(area_rectangulo(5.0, 3.0))
+    println(par_o_impar(16))
+    println(positivo(0))
 }
