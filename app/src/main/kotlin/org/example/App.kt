b144f68ef6a4148612d357517c814d301137c0db
Noah <noahlucianograna@gmail.com>
2025-04-25 09:42:47 -0300

    este es el paso dos pero con la tarea adicional

--- app/src/main/kotlin/org/example/App.kt
+++ app/src/main/kotlin/org/example/App.kt
@@ -1,13 +1,16 @@
 
 package org.example
-
+fun <T> mostrar_tipo(variable: T): String {
+    return "Variable de tipo: ${variable!!::class.simpleName}"
+}
+fun area_rectangulo(base: Double, altura: Double): String{
+    return "${base*altura}cm2"
+}
 fun main() {
-    fun <T> mostrar_tipo(variable: T): String {
-        return "Variable de tipo: ${variable!!::class.simpleName}"
-    }
     val nombre = "Noah"
     val num = 24
     val bool = true
     val decimal = 3.14
     println(mostrar_tipo(bool))
+    println(area_rectangulo(5.0, 3.0))
 }
